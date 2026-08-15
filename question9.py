15d7d1e3d5459775a05a7c3ce7dd584d5d8e8ea8
MythriReddy510 <my3gr2005@gmail.com>
2026-08-15 12:54:14 +0530

    Completed question 9

---
+++ question9.py
@@ -0,0 +1,10 @@
+# Message Slicing Tool
+
+s = input("Enter a message: ")
+
+print(f"First 5 characters: {s[:5]}")
+print(f"Last 5 characters: {s[-5:]}")
+print(f"Characters from Index 2 to 7: {s[2:7]}")
+print(f"Every Second Character: {s[::2]}")
+print(f"Message in Reverse: {s[::-1]}")
+print(f"Message without First and Last Character: {s[1:-1]}")
