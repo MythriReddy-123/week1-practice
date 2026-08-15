6a1287da011a25fb85eff5c9ad2d6b7ae7d869d5
MythriReddy510 <my3gr2005@gmail.com>
2026-08-15 12:56:23 +0530

    Completed question 9

--- question9.py
+++ question9.py
@@ -4,7 +4,7 @@ s = input("Enter a message: ")
 
 print(f"First 5 characters: {s[:5]}")
 print(f"Last 5 characters: {s[-5:]}")
-print(f"Characters from Index 2 to 7: {s[2:7]}")
+print(f"Characters from Index 2 to 7: {s[2:8]}")
 print(f"Every Second Character: {s[::2]}")
 print(f"Message in Reverse: {s[::-1]}")
 print(f"Message without First and Last Character: {s[1:-1]}")
